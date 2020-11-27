68804ef77763d4d91d64250a96711ac2144470c9
Stu Field <stu.g.field@gmail.com>
2020-11-27 09:03:34 -0700

    Add comments to partial matching bug

--- partial_match_bug.R
+++ partial_match_bug.R
@@ -1,8 +1,11 @@
 
 
-
-wrapper <- function(x, y, main.cex=2, ...) {
+# partial matching of arguments can be problematic when
+# wrapping functions
+wrapper <- function(x, y, main.cex = 2, ...) {
 	plot(x, y)
-	print(match.call(expand.dots=TRUE))
-	title(..., cex.main=main.cex)
+	print(match.call(expand.dots = TRUE))
+	title(..., cex.main = main.cex)
 }
+
+wrapper(1, 2, main = "hello")
