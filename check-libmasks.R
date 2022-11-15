9dd846400b03530640eedf4ed365825e54c1d495
Stu Field <stu.g.field@gmail.com>
2022-11-15 09:35:41 -0700

    Update check_libmasks()

--- check-libmasks.R
+++ check-libmasks.R
@@ -1,20 +1,26 @@
 check_libmasks <- function() {
   libs <- .libPaths()
-  stopifnot(length(libs) >= 2L)
-  l1 <- list.dirs(libs[1L], full.names = FALSE, recursive = FALSE)
-  l2 <- list.dirs(libs[2L], full.names = FALSE, recursive = FALSE)
-  masks <- intersect(l1, l2)
-  if ( length(masks) > 0 ) {
+  lib1 <- libs[1L]
+  lib2 <- libs[2L]
+  newlibs <- dir(lib1)
+  is_lib <- vapply(newlibs, function(.x)
+    #all(c("NAMESPACE", "DESCRIPTION", "Meta") %in% dir(file.path(lib1, .x))),
+    length(dir(file.path(lib1, .x), pattern = "Meta")) > 0,
+    NA)
+  newlibs <- newlibs[is_lib]
+  if ( length(newlibs) > 0 ) {
     message(
-      "You have over-installed packages!\n",
+      "Attention! You have over-installed packages!\n",
       "Potentially dangerous installations:"
     )
-    v1 <- lapply(masks, packageVersion, lib.loc = libs[1L]) |> sapply(as.character)
-    v2 <- lapply(masks, packageVersion, lib.loc = libs[2L]) |> sapply(as.character)
-    df <- data.frame(p = masks, l1 = package_version(v1),
-                         x = "\u2026", l2 = package_version(v2, strict = FALSE))
+    .f <- function(...) tryCatch(as.character(utils::packageVersion(...)),
+                                 error = function(e) NA_character_)
+    v1 <- vapply(newlibs, .f, lib.loc = lib1, "", USE.NAMES = FALSE)
+    v2 <- vapply(newlibs, .f, lib.loc = lib2, "", USE.NAMES = FALSE)
+    df <- data.frame(p = newlibs, l1 = package_version(v1),
+                     x = "\u2026", l2 = package_version(v2, strict = FALSE))
     df$y <- ifelse(df$l1 > df$l2, "\u2713",  "\u2716")
-    names(df) <- c("package", libs[1L], " ", libs[2L], "newer")
+    names(df) <- c("package", lib1, " ", lib2, "newer")
     str <- structure("<pkg_vrs>", class = c("package_version", "numeric_version"))
     r1  <- data.frame("<chr>", str, "", str, "<chr>", check.names = FALSE)
     names(r1) <- names(df)
