3c782b479f040d908714681fee25f20037da4f32
Stu Field <stu.g.field@gmail.com>
2024-11-19 13:10:12 -0700

    Rename alignVectors() -> align_vecs()

--- align-vecs.R
+++ align-vecs.R
@@ -7,11 +7,11 @@
 #'   lapply(c(4, 5, 8, 7, 3), function(.x) sample(LETTERS[1:10], .x)),
 #'   letters[1:5]
 #' )
-#' alignVectors(x)
+#' align_vecs(x)
 #' x <- setNames(as.list(LETTERS), letters)
-#' alignVectors(x)
+#' align_vecs(x)
 #' @export
-alignVectors <- function(x) {
+align_vecs<- function(x) {
   stopifnot(inherits(x, "list"))
   jagged <- function(.x) tibble::as_tibble(lapply(.x, "length<-", max(lengths(.x))))
   all <- unique(unlist(x))
