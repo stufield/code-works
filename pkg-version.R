c4a89061e59b28b9c3a80fb1b2c82fce85d792f4
Stu Field <stu.g.field@gmail.com>
2024-11-19 13:07:42 -0700

    Rename pkgVersion() -> pkg_version()

--- pkg-version.R
+++ pkg-version.R
@@ -1,9 +1,9 @@
 
-pkgVersion <- function(pkg) {
+pkg_version <- function(pkg) {
   .f <- function(...) tryCatch(as.character(utils::packageVersion(...)),
                                error = function(e) NA_character_)
   vapply(.libPaths(), .f, pkg = pkg, "") |>
     package_version(strict = FALSE) |>
     tibble::enframe(name = "lib", value = "version")
 }
-pkgVersion("SomaReadr")
+pkg_version("helpr")
