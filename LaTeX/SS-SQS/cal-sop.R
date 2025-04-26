00269e27efbb97e9318dae47a63c0df5edc180cf
Stu Field <stu.g.field@gmail.com>
2025-04-25 18:06:10 -0600

    Added dependencies that were missing

---
+++ LaTeX/SS-SQS/cal-sop.R
@@ -0,0 +1,137 @@
+#' Calibration SOP
+#'
+#' Calculate SOP calibration acceptance criteria for
+#' all analytes in a `soma_adat` object.
+#'
+#' @inheritParams hybNormalize
+#' @param adat A `soma_adat` object with RFU values to be
+#'   tested for SOP acceptance.
+#' @param cals An optional data frame of calibration scale factors. If
+#'   `NULL`, `cals` is created via [getCalSFs()].
+#' @param apt.data An "apt.data" object which contains calibration scale factor
+#'   data and dilution data. Usually from a call to [getAnalyteInfo()].
+#' @param legend.cex Character expansion for the plot legend (if `plot = TRUE`).
+#' @param do.plot Logical. Include scale factors and acceptance criteria plots?
+#' @return An invisibly returned list of calibration SOP metrics:
+#'   \item{cal.table}{A table of calibration statistics for each plate.}
+#'   \item{tail.fails}{A list of the failed SOMAmers by plate.}
+#'   \item{tail.tbl}{A tibble of the failed SOMAmers by plate with scale factors.}
+#'   \item{plots}{A list of 2 `ggplots` for the median test and for the centered
+#'                tail test.}
+#' @author Stu Field
+#' @seealso [getCalSFs()], [plotCalSFs()]
+#' @examples
+#' out <- calibrationSOP(plasma20_hyb_med_cal)
+#'
+#' # `ggplot2` print method performs plotting
+#' out
+#' @importFrom dplyr select filter all_of
+#' @importFrom tibble as_tibble
+#' @importFrom purrr map_if
+#' @export
+calibrationSOP <- function(adat = NULL, cals = NULL, apt.data = NULL,
+                           legend.cex = 1, do.plot = TRUE, verbose = interactive()) {
+
+  if ( is.null(cals) ) {
+    cals <- getCalSFs(adat, apt.data)
+  }
+
+  hyb_seqs  <- seqid2apt(seq_HybControlElution)
+  cals      <- dplyr::filter(cals, !AptName %in% hyb_seqs) # remove HybControls
+  cal_names <- grep("AptName|Dilution", names(cals), value = TRUE, invert = TRUE)
+  medians   <- dplyr::select(cals, all_of(cal_names)) |>
+    vapply(median, double(1))  # calc median cal sf by plate
+
+  # Median Test
+  # median must be between 0.8 - 1.2
+  median_fails <- cal_names[abs(medians - 1) > 0.2]
+
+  # Tail test
+  tails <- setNames(cal_names, cal_names) |>
+    lapply(function(.plate) {
+      cal_sfs        <- cals[[.plate]]                  # Cal scale factors
+      names(cal_sfs) <- cals$AptName
+      right.tail     <- cal_sfs[cal_sfs > medians[.plate] + 0.4]
+      left.tail      <- cal_sfs[cal_sfs < medians[.plate] - 0.4]
+      c(left.tail, right.tail)
+      # ------- ALTERNATIVE WAY OF SETTING TAIL WEIGHT CRITERION #
+      # nolint start
+      # cal_sfs <- log2(cal_sfs)
+      # cal_sfs <- cal_sfs - median(cal_sfs)
+      # cal_sfs[abs(cal_sfs) > log2(1.4)]
+      # nolint end
+    })
+
+  # which plates > 5%
+  lgl <- vapply(tails, function(.x) length(.x) / nrow(cals) > 0.05, NA)
+  tail_fails <- cal_names[lgl]
+
+  if ( do.plot ) {
+    plural <- ifelse(length(cal_names) > 1L, "s", "")
+    p <- list()
+    p$median_test <- plotCalSFs(
+      apt.data    = cals,
+      ablines     = c(0.8, 1.2),
+      by.dilution = FALSE,
+      main        = sprintf("Median Test: Median%s within (0.8-1.2)", plural)
+    )
+
+    # Now center the distribution on 1 and look at tails
+    center_cals <- cals |>
+      purrr::map_if(is.numeric, function(.x) .x - median(.x) + 1) |>
+      as_tibble()
+    p$tail_test <- plotCalSFs(center_cals, apt.data = center_cals,
+                              ablines = c(0.6, 1.4), by.dilution = FALSE,
+                              main = "Tail Test: 95% median +/- 0.4")
+  } else {
+    p <- NULL
+  }
+
+  # not sure why these are here, they don't seem to do anything; sgf
+  if ( length(median_fails) > 0L ) {
+    lapply(median_fails, function(fail) {
+      sprintf("\t%s: %0.4f", fail, medians[fail])
+    })
+  }
+  if ( length(tail_fails) > 0L ) {
+    lapply(cal_names, function(.plate) {
+      sprintf("\t%s: %0.4f", .plate, length(tails[[.plate]]) / nrow(cals))
+    })
+  }
+
+  counts     <- lengths(tails)
+  percs      <- counts / nrow(cals) * 100
+  tail_percs <- rbind(counts, percs) |> t() |> data.frame() |>
+    setNames(c("Proteins in Tails", "Percent in Tails"))
+
+  tail_percs[["Median Scale Factor"]] <- medians
+  tail_percs[["Median Shift"]] <- 1 - medians
+  tail_percs <- tail_percs[, sort(names(tail_percs)) ]
+
+  fails <- setNames(cal_names, cal_names) |>
+    lapply(function(.plate) names(tails[[.plate]]))
+
+  if ( verbose ) {
+    pad <- 45
+    msg <- pad("No. plates failing the median shift criterion (0.8-1.2)", pad)
+    signal_todo(msg, value(length(median_fails)))
+    msg <- pad("No. plates failing the tail criterion (95%% median +/- 0.4)", pad)
+    signal_todo(msg, value(length(tail_fails)))
+    msg <- pad("No. SOMAmers failing tail criterion", pad)
+    signal_todo(msg, value(length(unique(unlist(fails)))))
+  }
+
+  # subset all failed apts
+  tail_tbl <- rearrange(cals, "AptName", unique(unlist(fails)))
+  # sorting variable
+  tail_tbl$sort <- apply(tail_tbl[, cal_names], 1, function(.x) sum(abs(log(.x))))
+  tail_tbl      <- dplyr::arrange(tail_tbl, dplyr::desc(sort)) # sort by new variables
+  tail_tbl$fail.rank <- seq_len(nrow(tail_tbl))                # create ranks column
+
+  invisible(
+    list(cal.table  = tail_percs,
+         tail.fails = fails,
+         tail.tbl   = tail_tbl,
+         plots      = p)
+  )
+}
---
+++ LaTeX/SS-SQS/plot-scale-factors.R
@@ -0,0 +1,85 @@
+#' Median Normalization Plots
+#'
+#' A quick plotting routine for the visualization of ADAT median
+#' normalization scale factors. Produces boxplots of all 3 dilution mixes
+#' by the `group =` argument.
+#'
+#' @family Plotting
+#' @param data A `soma_adat` or `data.frame` object containing columns
+#'   of median normalization scale factors to plot.
+#' @param group Column name by which to split the boxplots.
+#' @param drop.hyb Logical. Should hybridization scale factors also be plotted?
+#' @param do.cdf Logical. Should CDFs be plotted instead? Many boxplot specific
+#'   arguments will be ignored.
+#' @param notch Logical. Passed to [geom_boxplot()].
+#' @return A series of boxplots or CDFs of the median normalization scale
+#'   factors.
+#' @author Stu Field
+#' @seealso [geom_boxplot()], [stat_ecdf()]
+#' @examples
+#' adat <- dplyr::filter(plasma20_hyb_med, SampleType == "Sample")
+#' plotMedNorm(adat)   # defaults
+#'
+#' plotMedNorm(adat, group = "TimePoint")
+#'
+#' plotMedNorm(adat, notch = FALSE)  # silence notch warning
+#'
+#' plotMedNorm(adat, do.cdf = TRUE)
+#' @importFrom SomaPlot theme_soma
+#' @importFrom ggplot2 ggplot geom_boxplot geom_hline
+#' @importFrom ggplot2 geom_vline aes geom_jitter scale_color_manual
+#' @importFrom ggplot2 scale_fill_manual scale_x_discrete
+#' @importFrom ggplot2 ylab facet_wrap ggtitle stat_ecdf
+#' @export
+plotMedNorm <- function(data, group = "SampleGroup", drop.hyb = TRUE,
+                        do.cdf = FALSE, notch = TRUE) {
+
+  medsf <- sort(getNormNames(data, drop.hyb = drop.hyb))
+
+  if ( length(medsf) == 0L ) {
+    stop(
+      "No median scale factors found in adat. ",
+      "Are you sure this data has been median normalized?",
+      call. = FALSE
+    )
+  }
+
+  col_pal <- unlist(soma_colors, use.names = FALSE)
+
+  if ( do.cdf ) {
+    gg <- refactorData(data) |>
+      dplyr::select(all_of(medsf), group) |>
+      tidyr::gather(key = Mix, value = value, -!!group) |>
+      dplyr::mutate(Mix = factor(Mix, levels = medsf)) |>
+      ggplot(aes(x = value, colour = !!dplyr::sym(group))) +
+      stat_ecdf(size = 0.7) +
+      scale_color_manual(values = col_pal) +
+      labs(title = "eCDF Median Normalization Scale Factors",
+           x = "Scale Factor",
+           y = bquote(italic(P) ~ (X < x))) +
+      geom_vline(xintercept = c(0.4, 2.5), linetype = "dashed") +
+      facet_wrap(~Mix, scales = "free_y") +
+      theme_soma() +
+      NULL
+  } else {
+    gg <- refactorData(data) |>
+      dplyr::select(all_of(medsf), group) |>
+      tidyr::gather(key = Mix, value = value, -!!group) |>
+      dplyr::mutate(Mix = factor(Mix, levels = medsf)) |>
+      ggplot(aes(y = value, x = !!dplyr::sym(group),
+                 fill = !!dplyr::sym(group))) +
+      geom_boxplot(color = "#1F3552", alpha = 0.7,
+                   outlier.color = NA,   # no outliers, jitter instead
+                   notch = notch, size = 0.5) +
+      scale_fill_manual(values = col_pal) +
+      geom_jitter(width = 0.05, alpha = 0.5, size = 2.5) +
+      scale_x_discrete(name = group) +
+      geom_hline(yintercept = c(0.4, 2.5), linetype = "dashed") +
+      ylab("Scale Factor") +
+      ggtitle("Median Normalization Scale Factors") +
+      facet_wrap(~Mix) +
+      theme_soma() +
+      NULL
+  }
+  gg
+}
